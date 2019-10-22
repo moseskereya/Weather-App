0c628f6a4762ef756e1b54452e3aa58bbe96b7bb
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-22 17:35:05 +0300

    Updates

---
+++ precache-manifest.a6e3a3bac083df81d9afef886b56f4e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d03fb6b18ebb5d4a4c6f8f1a300d93",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "5450e8d8597fe7698b23",
+    "url": "/Weather-App/static/css/main.eb03fb19.chunk.css"
+  },
+  {
+    "revision": "9b03ecf1177bcaa13e44",
+    "url": "/Weather-App/static/js/2.d1d18a05.chunk.js"
+  },
+  {
+    "revision": "5450e8d8597fe7698b23",
+    "url": "/Weather-App/static/js/main.e3a95a72.chunk.js"
+  },
+  {
+    "revision": "4c8f9ec088fedf7c5d10",
+    "url": "/Weather-App/static/js/runtime-main.9cd3f58b.js"
+  }
+]);
